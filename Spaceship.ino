1efc746275deb53d2a92f0085e4f7ee100284e9a
Nper4lta <55100434+Nper4lta@users.noreply.github.com>
2020-11-11 14:56:24 -0800

    Update Spaceship.ino

--- Spaceship.ino
+++ Spaceship.ino
@@ -19,12 +19,12 @@ void loop()
   switchState = digitalRead(10);
 
   if (switchState == HIGH) {
-    tone(buzzer,1000);
-    myservo.write(90);
-    digitalWrite(5, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(3, LOW);
-    
+    tone(buzzer,1000); //Makes the sound
+    myservo.write(90); //Moves the servo
+    digitalWrite(5, HIGH); //Red Light 1
+    digitalWrite(4, HIGH); //Red Light 2
+    digitalWrite(3, LOW); //Green Light
+    //Makes the blinking
     delay(1000);
     digitalWrite(5, HIGH);
     digitalWrite(4, LOW);
@@ -33,8 +33,8 @@ void loop()
 
   }
   else {
-    myservo.write(0);
-    noTone(buzzer);
+    myservo.write(0); //Puts servo to 0
+    noTone(buzzer); //stops sound
     digitalWrite(3, HIGH);
     delay(250);
     digitalWrite(5, LOW);
